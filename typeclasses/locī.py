1b26d301f0278923d98efbbe85c35abf323b4095
jamalainm <robot@inman.xyz>
2021-02-19 14:52:43 -0600

    Added two new typeclasses for dealing with Latin language rooms and exits ('locī.py' and 'exitūs.py', respectively).

---
+++ typeclasses/locī.py
@@ -0,0 +1,34 @@
+"""
+Room
+
+Rooms are simple containers that has no location of their own.
+
+"""
+
+from evennia import DefaultRoom
+from utils.latin.latin_declension import DeclineNoun
+from utils.latin.populate_forms import populate_forms
+from typeclasses.latin_noun import LatinNoun
+
+class Locus(DefaultRoom,LatinNoun):
+    """
+    Rooms are like any Object, except their location is None
+    (which is default). They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    def at_object_creation(self):
+
+        # add all of the case endings to attributes
+
+        nom = self.db.formae['nom_sg'][0]
+        gen = self.db.formae['gen_sg'][0]
+        sexus = self.db.sexus
+
+        populate_forms(self, nom, gen, sexus)
+
+        self.db.lang = 'latin'
